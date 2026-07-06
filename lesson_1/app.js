473236f2ef8e85cd1fb77cc1a44d48022b83ded9
Rinat <258747231+rinatwebric@users.noreply.github.com>
2026-07-06 21:25:59 +0600

    Урок 1
    типы данных 
    операции

---
+++ lesson_1/app.js
@@ -0,0 +1,40 @@
+console.log("Hello, rico!");
+
+console.warn("This is a warning message.");
+
+console.error("This is fake error message.");
+
+// Data types in JavaScript
+
+// camell case
+// snake case
+// pascal case
+// kebab case
+// 1. String
+let myName = "Rinat";
+let mySurname = "Shadiev";
+let myFatherName = "Talaibekovich";
+// 2. Number
+let myAge = 19;
+// 3. Boolean
+let isStudent = true;
+let myFullName = mySurname + " " + myName + " " + myFatherName; // Concatenation
+// 4. null
+let myNull = null;
+// 5. object
+// 6. undefined
+// 7. bigint
+// 8. symbol
+console.log(myFullName);
+
+let mbankName = myName + " " + mySurname[0] + ".";
+
+console.log(mbankName);
+
+
+let optimeName = mySurname[0] + "." + myName[0] + "." + myFatherName[0];
+
+console.log(optimeName);
+
+// let const var
+
